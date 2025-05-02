b0be5705d6ac5675ffa67c099c84093c64ca4fd6
Alberto Martinez Muñoz <alberto.martinez@getdrop.com>
2025-05-02 09:21:29 +0200

    feat: add communcations layers chain of reponsability when sending messages

--- src/message-manager.cpp
+++ src/message-manager.cpp
@@ -1,28 +1,25 @@
 #include <stdio.h>
 
-class socket_application_layer {
-    public:
-        socket_application_layer(){}
-};
-
-class slip_transport_layer {
-    public:
-        slip_transport_layer(){}
-};
+#include <communications-layer/communications-layer.hpp>
+#include <communications-layer/socket-application-layer.hpp>
+#include <communications-layer/slip-transport-layer.hpp>
 
 class message_manager {
     public:
-        message_manager(socket_application_layer *application_layer, slip_transport_layer *transport_layer)
+        message_manager(communications_layer *application_layer, communications_layer *transport_layer)
         : application_layer(application_layer), transport_layer(transport_layer){
+            this->application_layer->set_next_layer(this->transport_layer);
         }
 
         int send_fast_command(){
             printf("command_manager: send_fast_command\r\n");
+            this->application_layer->send_message((uint8_t*)"fast_command");
             return 0;
         }
 
         int send_slow_command(){
             printf("command_manager: send_slow_command\r\n");
+            this->application_layer->send_message((uint8_t*)"slow_command");
             return 0;
         }
 
@@ -32,15 +29,15 @@ class message_manager {
         }
 
     private:
-        socket_application_layer *application_layer;
-        slip_transport_layer *transport_layer;
+        communications_layer *application_layer;
+        communications_layer *transport_layer;
 };
 
 int main() {
     printf("-------------------- Message Manager v2025.05.01 --------------------\r\n");
 
-    slip_transport_layer *transport_layer = new slip_transport_layer();
-    socket_application_layer *application_layer = new socket_application_layer();
+    slip_transport_layer *application_layer = new slip_transport_layer();
+    socket_application_layer *transport_layer = new socket_application_layer();
     message_manager *msg_manager = new message_manager(application_layer, transport_layer);
 
     msg_manager->send_fast_command();
