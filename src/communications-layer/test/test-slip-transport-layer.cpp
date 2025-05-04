1ff87cb725b96c214a541aa5fe8f1a588d693f0e
Alberto Martinez Muñoz <alberto.martinez@getdrop.com>
2025-05-04 19:38:52 +0200

    test(slip-layer): change normal tests to parametrized tests

--- src/communications-layer/test/test-slip-transport-layer.cpp
+++ src/communications-layer/test/test-slip-transport-layer.cpp
@@ -3,104 +3,137 @@
 #include <communications-layer/slip-transport-layer.hpp>
 #include <cstdlib>
 #include <cstring>
+#include <vector>
 
 using testing::ElementsAreArray;
 
-class mock_application_layer : public communications_layer {
-    public:
-        uint8_t *message = nullptr;
+class mock_application_layer : public communications_layer
+{
+public:
+    uint8_t *message = nullptr;
 
-        mock_application_layer(){}
+    mock_application_layer() {}
 
-        int send_message(uint8_t *message, size_t size) override {
-            this->message = (uint8_t *)realloc(this->message, size);
-            if(this->message == nullptr)
-            {
-                perror("malloc");
-                return -1;
-            }
-            memcpy(this->message, message, size);
-            return send_message_return;
+    int send_message(uint8_t *message, size_t size) override
+    {
+        this->message = (uint8_t *)realloc(this->message, size);
+        if (this->message == nullptr)
+        {
+            perror("malloc");
+            return -1;
         }
+        memcpy(this->message, message, size);
+        return send_message_return;
+    }
 
-        void set_send_message_return(int return_value) {
-            send_message_return = return_value;
-        }
+    void set_send_message_return(int return_value)
+    {
+        send_message_return = return_value;
+    }
 
-        ~mock_application_layer() {
-            free(message);
-            message = nullptr;
-        }
-    
-    private:
-        int send_message_return = 0;
+    ~mock_application_layer()
+    {
+        free(message);
+        message = nullptr;
+    }
+
+private:
+    int send_message_return = 0;
 };
 
-class TestSlipTransportLayer: public ::testing::Test {
+class TestSlipTransportLayerParams
+{
+public:
+    TestSlipTransportLayerParams(std::string name, int next_handler_return, uint8_t *msg, size_t msg_size, uint8_t *expected_msg, size_t expected_msg_size) : name(name), next_handler_return(next_handler_return), msg(msg), msg_size(msg_size), expected_msg(expected_msg), expected_msg_size(expected_msg_size) {}
+
+    static void *operator new(std::size_t count)
+    {
+        void *p = ::operator new(count);
+        memset(p, 0, count);
+        return p;
+    }
+
+    static void *operator new[](std::size_t count)
+    {
+        void *p = ::operator new[](count);
+        memset(p, 0, count);
+        return p;
+    }
+
+    std::string name;
+    int next_handler_return;
+    uint8_t *msg;
+    size_t msg_size;
+    uint8_t *expected_msg;
+    size_t expected_msg_size;
+};
+
+uint8_t simple_msg[4] = {0x01, 0x05, 0x06, 0x10};
+uint8_t expected_simple_msg[5] = {0x01, 0x05, 0x06, 0x10, slip_transport_layer::END};
+uint8_t escape_end_msg[4] = {0x01, 0x05, slip_transport_layer::END, 0x10};
+uint8_t expected_escape_end_msg[6] = {0x01, 0x05, slip_transport_layer::ESC, slip_transport_layer::ESC_END, 0x10, slip_transport_layer::END};
+uint8_t escape_esc_msg[4] = {0x01, 0x05, slip_transport_layer::ESC, 0x10};
+uint8_t expected_escape_esc_msg[6] = {0x01, 0x05, slip_transport_layer::ESC, slip_transport_layer::ESC_ESC, 0x10, slip_transport_layer::END};
+
+class TestSlipTransportLayer : public testing::TestWithParam<TestSlipTransportLayerParams>
+{
 public:
     slip_transport_layer *layer = nullptr;
     mock_application_layer *mock_layer = nullptr;
 
-	virtual void SetUp() {
-		layer = new slip_transport_layer();
+    virtual void SetUp()
+    {
+        layer = new slip_transport_layer();
         mock_layer = new mock_application_layer();
         mock_layer->set_send_message_return(0);
         layer->set_next_layer(mock_layer);
-	}
+    }
 
-	virtual void TearDown() {
+    virtual void TearDown()
+    {
         delete mock_layer;
-		delete layer;
-	}
+        delete layer;
+    }
 };
 
-TEST_F(TestSlipTransportLayer, WhenSendingADatagramIfOkItShouldAddSlipEnd) {
-	uint8_t message[] = {0x01, 0x05, 0x06, 0x10};
-    uint8_t expected_message[] = {0x01, 0x05, 0x06, 0x10, slip_transport_layer::END};
+TEST_P(TestSlipTransportLayer, TestNextSendHandlerMessage)
+{
+    std::vector<uint8_t> v;
+    v.assign(GetParam().expected_msg, GetParam().expected_msg + GetParam().expected_msg_size);
 
-    ASSERT_EQ(0, layer->send_message(message, sizeof(message)));
-    ASSERT_THAT(expected_message, ElementsAreArray(mock_layer->message,sizeof(expected_message)));
+    mock_layer->set_send_message_return(GetParam().next_handler_return);
+    ASSERT_EQ(GetParam().next_handler_return, layer->send_message(GetParam().msg, GetParam().msg_size));
+    ASSERT_THAT(v, ElementsAreArray(mock_layer->message, GetParam().expected_msg_size));
 }
 
-TEST_F(TestSlipTransportLayer, WhenSendingTwoDatagramIfOkItShouldAddSlipEnd) {
-	uint8_t first_message[] = {0x01, 0x05, 0x06, 0x10};
-    uint8_t second_message[] = {0x01, 0x05, 0x06, 0x10, 0x50};
+INSTANTIATE_TEST_SUITE_P(
+    ,
+    TestSlipTransportLayer,
+    ::testing::Values(
+        TestSlipTransportLayerParams{"WhenSendingADatagramIfOkItShouldAddSlipEnd", 0, simple_msg, sizeof(simple_msg), expected_simple_msg, sizeof(expected_simple_msg)},
+        TestSlipTransportLayerParams{"WhenSendingADatagramIf0xc0PresentItShouldChangeTo0xdb0xdc", 0, escape_end_msg, sizeof(escape_end_msg), expected_escape_end_msg, sizeof(expected_escape_end_msg)},
+        TestSlipTransportLayerParams{"WhenSendingADatagramIf0xdbPresentItShouldChangeTo0xdb0xdd", 0, escape_esc_msg, sizeof(escape_esc_msg), expected_escape_esc_msg, sizeof(expected_escape_esc_msg)},
+        TestSlipTransportLayerParams{"WhenSendingADatagramIfNextReturnErrorItShouldReturnError", -1, simple_msg, sizeof(simple_msg), expected_simple_msg, sizeof(expected_simple_msg)}),
+    [](const testing::TestParamInfo<TestSlipTransportLayerParams> &info)
+    {
+        return info.param.name;
+    });
+
+TEST_F(TestSlipTransportLayer, WhenSendingTwoDatagramIfOkItShouldAddSlipEnd)
+{
+    uint8_t first_message[] = {0x01, 0x05, 0x06, 0x10};
+    uint8_t second_message[] = {0x01, 0x05, 0x06, 0x11, 0x50};
     uint8_t expected_first_message[] = {0x01, 0x05, 0x06, 0x10, slip_transport_layer::END};
-    uint8_t expected_second_message[] = {0x01, 0x05, 0x06, 0x10, 0x50, slip_transport_layer::END};
-    
+    uint8_t expected_second_message[] = {0x01, 0x05, 0x06, 0x11, 0x50, slip_transport_layer::END};
+
     ASSERT_EQ(0, layer->send_message(first_message, sizeof(first_message)));
-    ASSERT_THAT(expected_first_message, ElementsAreArray(mock_layer->message,sizeof(expected_first_message)));
+    ASSERT_THAT(expected_first_message, ElementsAreArray(mock_layer->message, sizeof(expected_first_message)));
     ASSERT_EQ(0, layer->send_message(second_message, sizeof(second_message)));
-    ASSERT_THAT(expected_second_message, ElementsAreArray(mock_layer->message,sizeof(expected_second_message)));
+    ASSERT_THAT(expected_second_message, ElementsAreArray(mock_layer->message, sizeof(expected_second_message)));
 }
 
-TEST_F(TestSlipTransportLayer, WhenSendingADatagramIf0xc0PresentItShouldChangeTo0xdb0xdc) {
-	uint8_t message[] = {0x01, 0x05, slip_transport_layer::END, 0x10};
-    uint8_t expected_message[] = {0x01, 0x05, slip_transport_layer::ESC, slip_transport_layer::ESC_END, 0x10, slip_transport_layer::END};
-
-    ASSERT_EQ(0, layer->send_message(message, sizeof(message)));
-    ASSERT_THAT(expected_message, ElementsAreArray(mock_layer->message,sizeof(expected_message)));
-}
-
-TEST_F(TestSlipTransportLayer, WhenSendingADatagramIf0xdbPresentItShouldChangeTo0xdb0xdd) {
-	uint8_t message[] = {0x01, 0x05, slip_transport_layer::ESC, 0x10};
-    uint8_t expected_message[] = {0x01, 0x05, slip_transport_layer::ESC, slip_transport_layer::ESC_ESC, 0x10, slip_transport_layer::END};
-
-    ASSERT_EQ(0, layer->send_message(message, sizeof(message)));
-    ASSERT_THAT(expected_message, ElementsAreArray(mock_layer->message,sizeof(expected_message)));
-}
-
-TEST_F(TestSlipTransportLayer, WhenSendingADatagramIfNextReturnErrorItShouldReturnError) {
-	uint8_t message[] = {0x01, 0x05, slip_transport_layer::ESC, 0x10};
-    uint8_t expected_message[] = {0x01, 0x05, slip_transport_layer::ESC, slip_transport_layer::ESC_ESC, 0x10, slip_transport_layer::END};
-    int expected_return = -1;
-
-    mock_layer->set_send_message_return(expected_return);
-    ASSERT_EQ(expected_return, layer->send_message(message, sizeof(message)));
-    ASSERT_THAT(expected_message, ElementsAreArray(mock_layer->message,sizeof(expected_message)));
-}
-
-int main(int argc, char **argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
