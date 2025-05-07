bd91fa3f780238b77f206d519d6f2e5451e47cdd
Alberto Martinez Muñoz <alberto.martinez@getdrop.com>
2025-05-07 11:18:45 +0200

    feat/added first basic receiver/sender executables

---
+++ src/message-connection.h
@@ -0,0 +1,11 @@
+#ifndef IOT_MICRO_FIRMWARE_SRC_MESSAGE_H_
+#define IOT_MICRO_FIRMWARE_SRC_MESSAGE_H_
+
+/*
+ * File connection.h
+ */
+
+#define SOCKET_NAME "/tmp/9Lq7BNBnBycd6nxy.socket"
+#define BUFFER_SIZE 12
+
+#endif // IOT_MICRO_FIRMWARE_SRC_MESSAGE_H_
