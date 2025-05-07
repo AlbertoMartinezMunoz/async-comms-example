7a253e040388c99075e30978711c9f6c9b77438a
Alberto Martinez Muñoz <alberto.martinez@getdrop.com>
2025-05-07 13:33:53 +0200

    feat: pass data socket fd in constructor

--- src/socket-comms/test/test-socket-comms.cpp
+++ src/socket-comms/test/test-socket-comms.cpp
@@ -34,7 +34,7 @@ class TestSocketComms : public ::testing::Test
 public:
     virtual void SetUp()
     {
-        layer = new socket_transport_layer();
+        layer = new socket_transport_layer(expected_socket_id);
         RESET_FAKE(write);
         RESET_FAKE(read);
         write_fake.custom_fake = write_mock;
@@ -48,13 +48,13 @@ public:
 
 protected:
     socket_transport_layer *layer;
+    const int expected_socket_id = 5;
 };
 
 TEST_F(TestSocketComms, WhenSendingIfOKThenSendAndReturnOK)
 {
-    int expected_socket_id = 5;
     char buffer[] = "TEST MESSAGE";
-    ASSERT_EQ(0, layer->send(expected_socket_id, buffer, sizeof(buffer)));
+    ASSERT_EQ(0, layer->send(buffer, sizeof(buffer)));
     ASSERT_EQ(1, write_fake.call_count);
     ASSERT_EQ(expected_socket_id, write_fake.arg0_val);
     ASSERT_EQ(sizeof(buffer), write_fake.arg2_val);
@@ -63,18 +63,16 @@ TEST_F(TestSocketComms, WhenSendingIfOKThenSendAndReturnOK)
 
 TEST_F(TestSocketComms, WhenSendingIfWriteErrorThenReturnError)
 {
-    int expected_socket_id = 5;
     char buffer[] = "TEST MESSAGE";
     RESET_FAKE(write);
     write_fake.return_val = -1;
-    ASSERT_EQ(-1, layer->send(expected_socket_id, buffer, sizeof(buffer)));
+    ASSERT_EQ(-1, layer->send(buffer, sizeof(buffer)));
 }
 
 TEST_F(TestSocketComms, WhenReceivingIfOKThenReadAndReturnReadBytes)
 {
-    int expected_socket_id = 5;
     char buffer[32];
-    ASSERT_EQ(sizeof(expected_read_buf), layer->receive(expected_socket_id, buffer, sizeof(buffer)));
+    ASSERT_EQ(sizeof(expected_read_buf), layer->receive(buffer, sizeof(buffer)));
     ASSERT_EQ(1, read_fake.call_count);
     ASSERT_EQ(expected_socket_id, read_fake.arg0_val);
     ASSERT_EQ(sizeof(buffer), read_fake.arg2_val);
@@ -83,11 +81,10 @@ TEST_F(TestSocketComms, WhenReceivingIfOKThenReadAndReturnReadBytes)
 
 TEST_F(TestSocketComms, WhenReceivingIfReadErrorThenReturnError)
 {
-    int expected_socket_id = 5;
     char buffer[32];
     RESET_FAKE(read);
     read_fake.return_val = -1;
-    ASSERT_EQ(-1, layer->receive(expected_socket_id, buffer, sizeof(buffer)));
+    ASSERT_EQ(-1, layer->receive(buffer, sizeof(buffer)));
 }
 
 int main(int argc, char **argv)
