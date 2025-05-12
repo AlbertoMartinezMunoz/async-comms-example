20a9f0175950d18ff894ac46d0a30c6a13d2ec9f
Alberto Martinez Muñoz <alberto.martinez@getdrop.com>
2025-05-12 15:29:42 +0200

    feat(command-manager): implement incoming command processing method

--- src/command-manager/include/command-manager/command-observer.hpp
+++ src/command-manager/include/command-manager/command-observer.hpp
@@ -1,16 +1,18 @@
 #ifndef IOT_MICRO_FIRMWARE_SRC_COMMAND_MANAGER_INCLUDE_COMMAND_MANAGER_COMMAND_OBSERVER_H_
 #define IOT_MICRO_FIRMWARE_SRC_COMMAND_MANAGER_INCLUDE_COMMAND_MANAGER_COMMAND_OBSERVER_H_
-#include <string>
 
 class fast_command_observer
 {
-    virtual int process_command(int ack) = 0;
-    // virtual ~fast_command_observer() {}
+public:
+    virtual int process_command() = 0;
+    virtual ~fast_command_observer() {}
 };
 
 class slow_command_observer
 {
-    virtual int process_command(int ack, std::string message) = 0;
+public:
+    virtual int process_command() = 0;
+    virtual ~slow_command_observer() {}
 };
 
 #endif // IOT_MICRO_FIRMWARE_SRC_COMMAND_MANAGER_INCLUDE_COMMAND_MANAGER_COMMAND_OBSERVER_H_
---
+++ src/command-manager/test/test-command-manager-recv-command.cpp
@@ -0,0 +1,129 @@
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+#include <command-manager/command-manager.hpp>
+
+using ::testing::_;
+using ::testing::DoAll;
+using ::testing::Mock;
+using ::testing::Return;
+using ::testing::SetArrayArgument;
+using ::testing::StrEq;
+
+class CommunicationsLayerMock : public communications_layer_interface
+{
+public:
+    MOCK_METHOD(communications_layer_interface *, set_next_layer, (communications_layer_interface * handler), (override));
+    MOCK_METHOD(int, send_message, (const void *message, size_t size), (override));
+    MOCK_METHOD(ssize_t, send, (const char *buffer, size_t buffer_size), (override));
+    MOCK_METHOD(ssize_t, recv, (char *buffer, size_t buffer_size), (override));
+};
+
+class FastCmdObseverMock : public fast_command_observer
+{
+public:
+    MOCK_METHOD(int, process_command, ());
+};
+
+class SlowCmdObseverMock : public slow_command_observer
+{
+public:
+    MOCK_METHOD(int, process_command, ());
+};
+
+class TestCommandManager : public ::testing::Test
+{
+public:
+    virtual void SetUp()
+    {
+        comms_layer_mock = new CommunicationsLayerMock();
+        fast_cmd_observer_mock = new FastCmdObseverMock();
+        slow_cmd_observer_mock = new SlowCmdObseverMock();
+        command_mng = new command_manager(comms_layer_mock);
+        command_mng->subscribe_fast_cmd(fast_cmd_observer_mock);
+        command_mng->subscribe_slow_cmd(slow_cmd_observer_mock);
+        memset(slow_response_buffer, 0, sizeof(slow_response_buffer));
+    }
+
+    virtual void TearDown()
+    {
+        delete command_mng;
+        delete fast_cmd_observer_mock;
+        delete slow_cmd_observer_mock;
+        delete comms_layer_mock;
+    }
+
+protected:
+    command_manager *command_mng;
+    CommunicationsLayerMock *comms_layer_mock;
+    FastCmdObseverMock *fast_cmd_observer_mock;
+    SlowCmdObseverMock *slow_cmd_observer_mock;
+    const char expected_fast_command[13] = "FAST COMMAND";
+    const char expected_slow_command[13] = "SLOW COMMAND";
+    const char expected_slow_command_ack_response[13] = "ACK-RESPONSE";
+    char slow_response_buffer[13];
+};
+
+TEST_F(TestCommandManager, WhenReceivingFastCommandIfAckThenResponseAndReturnAck)
+{
+    EXPECT_CALL(*comms_layer_mock, recv(_, _))
+        .Times(1)
+        .WillOnce(DoAll(SetArrayArgument<0>(expected_fast_command, expected_fast_command + sizeof(expected_fast_command)), Return(sizeof(expected_fast_command))));
+    EXPECT_CALL(*fast_cmd_observer_mock, process_command()).Times(1).WillOnce(Return(0));
+    EXPECT_CALL(*comms_layer_mock, send(StrEq(command_manager::ack), sizeof(command_manager::ack)))
+        .Times(1)
+        .WillOnce(Return(sizeof(command_manager::ack)));
+    ASSERT_EQ(0, command_mng->incoming_cmd());
+}
+
+TEST_F(TestCommandManager, WhenReceivingSlowCommandIfAckThenResponseAndReturnAck)
+{
+    EXPECT_CALL(*comms_layer_mock, recv(_, _))
+        .Times(1)
+        .WillOnce(DoAll(SetArrayArgument<0>(expected_slow_command, expected_slow_command + sizeof(expected_slow_command)), Return(sizeof(expected_slow_command))));
+    EXPECT_CALL(*slow_cmd_observer_mock, process_command()).Times(1).WillOnce(Return(0));
+    ;
+    EXPECT_CALL(*comms_layer_mock, send(StrEq(command_manager::ack), sizeof(command_manager::ack)))
+        .Times(1)
+        .WillOnce(Return(sizeof(command_manager::ack)));
+    ASSERT_EQ(0, command_mng->incoming_cmd());
+}
+
+TEST_F(TestCommandManager, WhenReceivingFastCommandIfNAckThenResponseAndReturnNAck)
+{
+    EXPECT_CALL(*comms_layer_mock, recv(_, _))
+        .Times(1)
+        .WillOnce(DoAll(SetArrayArgument<0>(expected_fast_command, expected_fast_command + sizeof(expected_fast_command)), Return(sizeof(expected_fast_command))));
+    EXPECT_CALL(*fast_cmd_observer_mock, process_command()).Times(1).WillOnce(Return(1));
+    EXPECT_CALL(*comms_layer_mock, send(StrEq(command_manager::nack), sizeof(command_manager::nack)))
+        .Times(1)
+        .WillOnce(Return(sizeof(command_manager::nack)));
+    ASSERT_EQ(0, command_mng->incoming_cmd());
+}
+
+TEST_F(TestCommandManager, WhenReceivingSlowCommandIfNAckThenResponseAndReturnNAck)
+{
+    EXPECT_CALL(*comms_layer_mock, recv(_, _))
+        .Times(1)
+        .WillOnce(DoAll(SetArrayArgument<0>(expected_slow_command, expected_slow_command + sizeof(expected_slow_command)), Return(sizeof(expected_slow_command))));
+    EXPECT_CALL(*slow_cmd_observer_mock, process_command()).Times(1).WillOnce(Return(1));
+    EXPECT_CALL(*comms_layer_mock, send(StrEq(command_manager::nack), sizeof(command_manager::nack)))
+        .Times(1)
+        .WillOnce(Return(sizeof(command_manager::nack)));
+    ASSERT_EQ(0, command_mng->incoming_cmd());
+}
+
+TEST_F(TestCommandManager, WhenReceivingFastCommandIfRecvErrorThenReturnError)
+{
+    int expected_error = -3;
+    EXPECT_CALL(*comms_layer_mock, recv(_, _))
+        .Times(1)
+        .WillOnce(Return(expected_error));
+    ASSERT_EQ(expected_error, command_mng->incoming_cmd());
+}
+
+int main(int argc, char **argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
