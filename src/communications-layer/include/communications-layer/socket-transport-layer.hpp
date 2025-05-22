200147cf6646878f8e3df76a233355d5a66a0f6a
Alberto Martinez Muñoz <alberto.martinez@getdrop.com>
2025-05-22 11:43:46 +0200

    feat(communications-layers-lib): add new socket listening method

--- src/communications-layer/include/communications-layer/socket-transport-layer.hpp
+++ src/communications-layer/include/communications-layer/socket-transport-layer.hpp
@@ -2,6 +2,7 @@
 #define IOT_MICRO_FIRMWARE_SRC_COMMUNICATIONS_LAYER_INCLUDE_COMMUNICATIONS_LAYER_SOCKET_TRANSPORT_LAYER_H_
 
 #include <communications-layer/communications-layer.hpp>
+#include <communications-layer/communications-observer.hpp>
 
 class socket_transport_layer : public communications_layer
 {
@@ -12,12 +13,18 @@ public:
 
     int disconnect_socket();
 
+    int disconnect();
+
+    int listen_connections(const char *socket_path, communications_layer_observer *in_msg_observer);
+
     ssize_t send(const char *buffer, size_t buffer_size) override;
 
     ssize_t recv(char *buffer, size_t buffer_size) override;
 
 private:
     int sending_socket;
+    int listening_socket;
+    char *socket_path;
 };
 
 #endif // IOT_MICRO_FIRMWARE_SRC_COMMUNICATIONS_LAYER_INCLUDE_COMMUNICATIONS_LAYER_SOCKET_TRANSPORT_LAYER_H_
--- src/communications-layer/src/socket-transport-layer.cpp
+++ src/communications-layer/src/socket-transport-layer.cpp
@@ -38,6 +38,87 @@ int socket_transport_layer::disconnect_socket()
     return 0;
 }
 
+int socket_transport_layer::disconnect()
+{
+    close(listening_socket);
+    unlink(socket_path);
+    free(socket_path);
+
+    return 0;
+}
+
+int socket_transport_layer::listen_connections(
+    const char *path,
+    communications_layer_observer *in_msg_observer)
+{
+    struct sockaddr_un name;
+
+    socket_path = strdup(path);
+
+    /* Create local socket. */
+    listening_socket = socket(AF_UNIX, SOCK_SEQPACKET, 0);
+    if (listening_socket == -1)
+    {
+        perror("listening_socket");
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * For portability clear the whole structure, since some
+     * implementations have additional (nonstandard) fields in
+     * the structure.
+     */
+    memset(&name, 0, sizeof(name));
+
+    /* Bind socket to socket name. */
+    name.sun_family = AF_UNIX;
+    strncpy(name.sun_path, socket_path, sizeof(name.sun_path) - 1);
+
+    int ret = bind(listening_socket, (const struct sockaddr *)&name,
+                   sizeof(name));
+    if (ret == -1)
+    {
+        perror("bind");
+        close(listening_socket);
+        unlink(socket_path);
+        exit(EXIT_FAILURE);
+    }
+
+    /*
+     * Prepare for accepting connections. The backlog size is set
+     * to 20. So while one request is being processed other requests
+     * can be waiting.
+     */
+
+    ret = listen(listening_socket, 20);
+    if (ret == -1)
+    {
+        perror("listen");
+        close(listening_socket);
+        unlink(socket_path);
+        exit(EXIT_FAILURE);
+    }
+
+    for (int i = 0; i < 5; i++)
+    {
+        sending_socket = accept(listening_socket, NULL, NULL);
+        if (sending_socket == -1)
+        {
+            perror("accept");
+            return -1;
+        }
+
+        in_msg_observer->incoming_message();
+
+        sleep(1);
+        close(sending_socket);
+    }
+
+    printf("Stop Listening\r\n");
+
+    return 0;
+}
+
 ssize_t socket_transport_layer::send(const char *buffer, size_t buffer_size)
 {
     printf("socket_transport_layer: send_message '%s'\r\n", buffer);
