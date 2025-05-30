c8f26e69642e96f1147959c1d7c111e5825f8a19
Alberto M <traquinedes@yahoo.es>
2025-05-30 13:06:27 +0200

    feat: develop interactive console library using command pattern

--- src/interactive-console/src/interactive-console.cpp
+++ src/interactive-console/src/interactive-console.cpp
@@ -1,1 +1,68 @@
 #include <interactive-console/interactive-console.hpp>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <readline/readline.h>
+#include <readline/history.h>
+
+interactive_console::interactive_console(void)
+    : shutdown_command(nullptr), fast_command(nullptr), slow_command(nullptr) {}
+
+void interactive_console::listen(void)
+{
+    static char *line_read = (char *)NULL;
+    bool run = true;
+
+    while (run)
+    {
+        /* Get a line from the user. */
+        line_read = readline("$ ");
+
+        /* If the line has any text in it,
+           save it on the history. */
+        if (line_read && *line_read)
+        {
+            add_history(line_read);
+            switch (line_read[0])
+            {
+            case 'D':
+                run = false;
+                if (shutdown_command)
+                    shutdown_command->execute();
+                break;
+            case 'F':
+                if (fast_command)
+                    fast_command->execute();
+                break;
+            case 'S':
+                if (slow_command)
+                    slow_command->execute();
+                break;
+            default:
+                printf("Unknown command '%s'\r\n", line_read);
+                break;
+            }
+        }
+
+        if (line_read)
+        {
+            free(line_read);
+            line_read = (char *)NULL;
+        }
+    }
+}
+
+void interactive_console::set_shutdown_command(interactive_console_command *cmd)
+{
+    shutdown_command = cmd;
+}
+
+void interactive_console::set_fast_command(interactive_console_command *cmd)
+{
+    fast_command = cmd;
+}
+
+void interactive_console::set_slow_command(interactive_console_command *cmd)
+{
+    slow_command = cmd;
+}
--- src/interactive-console/test/test-interactive-console.cpp
+++ src/interactive-console/test/test-interactive-console.cpp
@@ -1,24 +1,127 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 #include <interactive-console/interactive-console.hpp>
+#include <readline/readline.h>
+#include <readline/history.h>
 
+#include "fff.h"
 
-class TestInteractiveConsole : public ::testing::Test
+using ::testing::AtLeast;
+using ::testing::AnyNumber;
+
+DEFINE_FFF_GLOBALS;
+FAKE_VALUE_FUNC(char *, readline, const char *);
+FAKE_VOID_FUNC(add_history, const char *);
+
+char *readline_shutdown_mock(__attribute__((unused)) const char *prompt)
+{
+    return strdup("D");
+}
+
+char *readline_fast_mock(__attribute__((unused)) const char *prompt)
+{
+    return strdup("F");
+}
+
+char *readline_slow_mock(__attribute__((unused)) const char *prompt)
+{
+    return strdup("S");
+}
+
+typedef char *(*readline_ptr)(const char *);
+
+class InteractiveConsoleCommandMock : public interactive_console_command
 {
 public:
+    MOCK_METHOD(void, execute, (), (const, override));
+};
 
+class InteractiveConsoleWrapper : public interactive_console
+{
+public:
+    InteractiveConsoleWrapper()
+        : interactive_console() {}
+    typedef void (InteractiveConsoleWrapper::*SetCommand)(interactive_console_command *);
+};
+
+class TestInteractiveConsoleParams
+{
+public:
+    TestInteractiveConsoleParams(std::string name, readline_ptr readline_handler, InteractiveConsoleWrapper::SetCommand set_command_func)
+        : name(name), readline_handler(readline_handler), set_command_func(set_command_func) {}
+
+    static void *operator new(std::size_t count)
+    {
+        void *p = ::operator new(count);
+        memset(p, 0, count);
+        return p;
+    }
+
+    static void *operator new[](std::size_t count)
+    {
+        void *p = ::operator new[](count);
+        memset(p, 0, count);
+        return p;
+    }
+
+    std::string name;
+    readline_ptr readline_handler;
+    InteractiveConsoleWrapper::SetCommand set_command_func;
+};
+
+class TestInteractiveConsole : public testing::TestWithParam<TestInteractiveConsoleParams>
+{
+public:
     virtual void SetUp()
     {
+        RESET_FAKE(readline);
+        cmd_mock = new InteractiveConsoleCommandMock();
+        shutdown_cmd_mock = new InteractiveConsoleCommandMock();
+        console = new InteractiveConsoleWrapper();
     }
 
     virtual void TearDown()
     {
+        delete console;
+        delete cmd_mock;
+        delete shutdown_cmd_mock;
     }
+
+    InteractiveConsoleWrapper *console;
+    InteractiveConsoleCommandMock *cmd_mock;
+    InteractiveConsoleCommandMock *shutdown_cmd_mock;
 };
 
-TEST_F(TestInteractiveConsole, WhenIfThenOK)
+TEST_P(TestInteractiveConsole, WhenReceivesCommandIfOKThenExecutesTheCommand)
 {
-    ASSERT_EQ(0,0);
+    char *(*custom_fakes[])(const char *) = {GetParam().readline_handler,
+                                             readline_shutdown_mock};
+    SET_CUSTOM_FAKE_SEQ(readline, custom_fakes, 2);
+
+    InteractiveConsoleWrapper::SetCommand set_command_func = GetParam().set_command_func;
+    (console->*set_command_func)(cmd_mock);
+
+    EXPECT_CALL(*cmd_mock, execute()).Times(1);
+    EXPECT_CALL(*shutdown_cmd_mock, execute()).Times(AnyNumber());
+    console->listen();
+}
+
+INSTANTIATE_TEST_SUITE_P(
+    ,
+    TestInteractiveConsole,
+    ::testing::Values(
+        TestInteractiveConsoleParams{"WhenReceivesShutdownCommandIfOKThenExecutesTheShutdownCommand", readline_shutdown_mock, &interactive_console::set_shutdown_command},
+        TestInteractiveConsoleParams{"WhenReceivesFastCommandIfOKThenExecutesTheFastCommand", readline_fast_mock, &interactive_console::set_fast_command},
+        TestInteractiveConsoleParams{"WhenReceivesSlowCommandIfOKThenExecutesTheSlowCommand", readline_slow_mock, &interactive_console::set_slow_command}),
+    [](const testing::TestParamInfo<TestInteractiveConsoleParams> &info)
+    {
+        return info.param.name;
+    });
+
+TEST_F(TestInteractiveConsole, WhenReceivesShudtownCommandIfOKThenStopListeningAndShutdown)
+{
+    readline_fake.custom_fake = readline_shutdown_mock;
+    console->listen();
 }
 
 int main(int argc, char **argv)
