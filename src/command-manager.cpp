6da52cc76248a72f9e3b935c83ac9b6c62ca8aba
Alberto M <traquinedes@yahoo.es>
2025-06-30 13:55:04 +0200

    feat: use console stop method

--- src/command-manager.cpp
+++ src/command-manager.cpp
@@ -5,7 +5,6 @@
 #include <stdio.h>
 #include <thread>
 #include <argp.h>
-#include <signal.h>
 
 #include <arguments-parser/arguments-parser.hpp>
 #include <command-manager/command-manager.hpp>
@@ -40,19 +39,20 @@ public:
 class shutdown_cmd_processor : public command_observer
 {
 public:
-    shutdown_cmd_processor(socket_transport_layer *socket)
-        : socket(socket) {}
+    shutdown_cmd_processor(socket_transport_layer *socket, interactive_console *console)
+        : socket(socket), console(console) {}
 
     int process_command() override
     {
         printf("********************** Received Shutdown Command **********************\r\n");
         socket->stop_listening();
-        kill(getpid(), SIGUSR2);
+        console->stop();
         return 0;
     }
 
 private:
     socket_transport_layer *socket;
+    interactive_console *console;
 };
 
 int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
@@ -75,15 +75,17 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
 
     argparser->parse(argc, argv);
 
+    console = interactive_console::get_instance();
+
     transport_layer = new socket_transport_layer();
     application_layer = new slip_application_layer();
     application_layer->set_next_communications_layer(transport_layer);
 
     slow_cmd = new slow_cmd_processor();
     fast_cmd = new fast_cmd_processor();
-    shutdown_cmd = new shutdown_cmd_processor(transport_layer);
-    cmd_mngr = new command_manager(application_layer);
+    shutdown_cmd = new shutdown_cmd_processor(transport_layer, console);
 
+    cmd_mngr = new command_manager(application_layer);
     cmd_mngr->subscribe_slow_cmd(slow_cmd);
     cmd_mngr->subscribe_fast_cmd(fast_cmd);
     cmd_mngr->subscribe_shutdown_cmd(shutdown_cmd);
@@ -92,14 +94,13 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
     fast_cli_cmd = new fast_command(cmd_mngr, transport_layer, argparser->get_remote_path());
     slow_cli_cmd = new slow_command(cmd_mngr, transport_layer, argparser->get_remote_path());
 
-    console = interactive_console::get_instance();
     console->set_shutdown_command(shutdown_cli_cmd);
     console->set_fast_command(fast_cli_cmd);
     console->set_slow_command(slow_cli_cmd);
 
     std::thread t1(&interactive_console::listen, console);
     if (transport_layer->listen_connections(argparser->get_local_path(), cmd_mngr) != 0)
-        kill(getpid(), SIGUSR2);
+        console->stop();
     t1.join();
 
     delete cmd_mngr;
