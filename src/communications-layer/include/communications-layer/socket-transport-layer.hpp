59634d34aa9816daa10d0bb3881ea918b3e706f9
Alberto M <traquinedes@yahoo.es>
2025-07-01 17:32:30 +0200

    feat(communications-layer-lib): socket layer uses the self-pipe trick to wake up from select

--- src/communications-layer/include/communications-layer/socket-transport-layer.hpp
+++ src/communications-layer/include/communications-layer/socket-transport-layer.hpp
@@ -3,11 +3,14 @@
 
 #include <communications-layer/communications-layer.hpp>
 #include <communications-layer/communications-observer.hpp>
-#include <signal.h>
 
 class socket_transport_layer : public communications_layer
 {
 public:
+    socket_transport_layer();
+
+    ~socket_transport_layer();
+    
     int connect_socket(const char *socket_path);
 
     int disconnect_socket();
@@ -21,11 +24,12 @@ public:
     ssize_t recv(char *buffer, size_t buffer_size) override;
 
 private:
-    static void signal_handler(int sig);
-
     int sending_socket;
     int listening_socket;
+    bool is_listening;
     char *socket_path;
+
+    int wakeuppfd[2];
 };
 
-#endif // IOT_MICRO_FIRMWARE_SRC_COMMUNICATIONS_LAYER_INCLUDE_COMMUNICATIONS_LAYER_SOCKET_TRANSPORT_LAYER_H_
+#endif  // IOT_MICRO_FIRMWARE_SRC_COMMUNICATIONS_LAYER_INCLUDE_COMMUNICATIONS_LAYER_SOCKET_TRANSPORT_LAYER_H_
