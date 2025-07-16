8214c3225b08538e9187cde611bcf4d2532060bf
Alberto M <traquinedes@yahoo.es>
2025-07-16 16:58:18 +0200

    refactor(remote-command-manager-lib): update class names

--- src/remote-command-manager/include/remote-command-manager/remote-command-observer.hpp
+++ src/remote-command-manager/include/remote-command-manager/remote-command-observer.hpp
@@ -1,11 +1,11 @@
 #ifndef IOT_MICRO_FIRMWARE_SRC_REMOTE_COMMAND_MANAGER_INCLUDE_REMOTE_COMMAND_MANAGER_REMOTE_COMMAND_OBSERVER_H_
 #define IOT_MICRO_FIRMWARE_SRC_REMOTE_COMMAND_MANAGER_INCLUDE_REMOTE_COMMAND_MANAGER_REMOTE_COMMAND_OBSERVER_H_
 
-class command_observer
+class remote_command_observer
 {
 public:
     virtual int process_command() = 0;
-    virtual ~command_observer() {}
+    virtual ~remote_command_observer() {}
 };
 
 #endif // IOT_MICRO_FIRMWARE_SRC_REMOTE_COMMAND_MANAGER_INCLUDE_REMOTE_COMMAND_MANAGER_REMOTE_COMMAND_OBSERVER_H_
