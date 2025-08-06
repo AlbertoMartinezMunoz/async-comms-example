979d2cfb2465b1ca98b4ae8cbaa4def931c700e9
Alberto M <traquinedes@yahoo.es>
2025-08-06 10:54:25 +0200

    feat(commands-lib): add new local commands ids

--- src/commands/include/commands/commands.hpp
+++ src/commands/include/commands/commands.hpp
@@ -8,9 +8,14 @@ class command {
 
     static constexpr char ack[4] = "ACK";
     static constexpr char nack[5] = "NACK";
+
     static constexpr char remote_fast_cmd_id[] = "FAST COMMAND";
     static constexpr char remote_slow_cmd_id[] = "SLOW COMMAND";
     static constexpr char remote_shutdown_cmd_id[] = "SHUTDOWN COMMAND";
+
+    static constexpr char local_fast_cmd_id[] = "F";
+    static constexpr char local_slow_cmd_id[] = "S";
+    static constexpr char local_shutdown_cmd_id[] = "D";
 };
 
 #endif // IOT_MICRO_FIRMWARE_SRC_COMMANDS_INCLUDE_COMMANDS_COMMANDS_H_
