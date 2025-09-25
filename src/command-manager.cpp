561c306844d66faf3060d1274ae8425b8ab87397
Alberto M <traquinedes@yahoo.es>
2025-09-25 10:05:19 +0200

    feat(communications-layers-lib): local tcp socket listener transport layer class

--- src/command-manager.cpp
+++ src/command-manager.cpp
@@ -3,6 +3,7 @@
  */
 
 #include <argp.h>
+#include <memory>
 #include <stdio.h>
 #include <thread>
 
@@ -11,11 +12,15 @@
 #include <command-manager/command-remote-handler.hpp>
 #include <commands/commands-implementations.hpp>
 #include <communications-layer/communications-layer.hpp>
+#include <communications-layer/local-tcp-socket-listener-transport-layer.hpp>
 #include <communications-layer/slip-application-layer.hpp>
 #include <communications-layer/socket-transport-layer.hpp>
 #include <interactive-console/interactive-console.hpp>
 
 int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[]) {
+    std::unique_ptr<local_tcp_socket_listener_transport_layer> remote_listener;
+    std::unique_ptr<slip_application_layer> remote_comms;
+
     socket_transport_layer *transport_layer;
     slip_application_layer *application_layer;
     remote_slow_command *remote_slow_cmd;
@@ -37,20 +42,24 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
 
     console = interactive_console::get_instance();
 
+    remote_listener = std::make_unique<local_tcp_socket_listener_transport_layer>(argparser->get_local_path());
+    remote_comms = std::make_unique<slip_application_layer>();
+    remote_comms->set_next_communications_layer(remote_listener.get());
+
     transport_layer = new socket_transport_layer(argparser->get_remote_path());
     application_layer = new slip_application_layer();
     application_layer->set_next_communications_layer(transport_layer);
 
     remote_slow_cmd = new remote_slow_command();
     remote_fast_cmd = new remote_fast_command();
-    remote_shutdown_cmd = new remote_shutdown_command(transport_layer, console);
+    remote_shutdown_cmd = new remote_shutdown_command(remote_listener.get(), console);
 
-    remote_cmd_handler = new command_remote_handler(application_layer);
+    remote_cmd_handler = new command_remote_handler(remote_comms.get());
     remote_cmd_handler->subscribe_slow_cmd(remote_slow_cmd);
     remote_cmd_handler->subscribe_fast_cmd(remote_fast_cmd);
     remote_cmd_handler->subscribe_shutdown_cmd(remote_shutdown_cmd);
 
-    shutdown_local_cmd = new local_shutdown_command(application_layer, transport_layer, console);
+    shutdown_local_cmd = new local_shutdown_command(application_layer, remote_listener.get(), console);
     fast_local_cmd = new local_fast_command(application_layer);
     slow_local_cmd = new local_slow_command(application_layer);
 
@@ -60,7 +69,7 @@ int main(__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
     local_cmd_handler->subscribe_shutdown_cmd(shutdown_local_cmd);
 
     std::thread t1(&interactive_console::listen, console, local_cmd_handler);
-    if (transport_layer->listen_connections(argparser->get_local_path(), remote_cmd_handler) != 0)
+    if (remote_listener->listen(remote_cmd_handler) != 0)
         console->shutdown();
     t1.join();
 
