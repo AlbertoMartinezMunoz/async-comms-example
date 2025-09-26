890c57af8117944dc6e38a12df1886568d5b5218
Alberto M <traquinedes@yahoo.es>
2025-09-26 11:17:27 +0200

    feat(event-listeners): event listener interface

---
+++ src/event-listeners/include/event-listeners/event-listener.hpp
@@ -0,0 +1,14 @@
+#ifndef IOT_MICRO_FIRMWARE_SRC_EVENT_LISTENERS_INCLUDE_EVENT_LISTENERS_EVENT_LISTENER_H_
+#define IOT_MICRO_FIRMWARE_SRC_EVENT_LISTENERS_INCLUDE_EVENT_LISTENERS_EVENT_LISTENER_H_
+
+class event_listener_handler {
+    virtual void handle() = 0;
+};
+
+class event_listener {
+  public:
+    virtual int listen(event_listener_handler *handler) = 0;
+    virtual int stop() = 0;
+};
+
+#endif // IOT_MICRO_FIRMWARE_SRC_EVENT_LISTENERS_INCLUDE_EVENT_LISTENERS_EVENT_LISTENER_H_
